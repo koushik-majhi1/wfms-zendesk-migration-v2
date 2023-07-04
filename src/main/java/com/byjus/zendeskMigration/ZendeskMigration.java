09b5d36925765ed42c852495988b7244128a3f12
koushik-majhi1 <koushik.majhi1@byjus.com>
2023-07-04 16:56:22 +0530

    initial commit

---
+++ src/main/java/com/byjus/zendeskMigration/ZendeskMigration.java
@@ -0,0 +1,13 @@
+package com.byjus.zendeskMigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZendeskMigration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZendeskMigration.class, args);
+	}
+
+}
---
+++ src/test/java/com/byjus/zendeskMigration/ZendeskMigrationTests.java
@@ -0,0 +1,13 @@
+package com.byjus.zendeskMigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZendeskMigrationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
